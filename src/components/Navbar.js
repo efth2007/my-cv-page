5c689b5de2761e838ef749d4701dd12a93cbe455
efth2007 <efth2007@hotmail.com>
2020-12-08 19:03:11 +0100

    Profile component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default class Navbar extends Component {
                             CV
                         </Link>
                         <Link to='/' data-target='side-nav' className='sidenav-trigger'>
-                            <i className='material-icons'>menu</i>
+                            <i class='material-icons'>menu</i>
                         </Link>
                         <ul className='right hide-on-med-and-down'>
                             <li>
@@ -44,6 +44,33 @@ export default class Navbar extends Component {
                     </div>
                 </div>
             </nav>
+            <ul className='sidenav' id='side-nav'>
+                            <li>
+                                <Link to="/">
+                                    <i className='fas fa-home'></i> Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/skills">
+                                    <i className='fas fa-copy'></i> Skills
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/experience">
+                                    <i className='fas fa-id-badge'></i> Experience
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/education">
+                                    <i className='fas fa-graduation-cap'></i> Education
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/projects">
+                                    <i className='fas fa-address-card'></i> Projects
+                                </Link>
+                            </li>
+                        </ul>
             </>
         )
     }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
+import ImgProfile from '../images/dwight.jpg'
+ 
 export default class Profile extends Component {
     render(){
         return(
-          <div></div>
+          <div>
+            <div className='card'>
+              <div className='card-image'> 
+                <img className='activator' src={ImgProfile} alt='My Name' />
+                <Link className='btn-floating halfway-fab waves-effect wave-light blue'>
+                  <i className='material-icons activator'>more_vert</i>
+                </Link>
+              </div>
+              <div className='card-content'>
+                <span className='card-title activator grey-text text-darken-4'>
+                  My Name
+                </span>
+                <p>Frontend Developer</p>
+              </div>
+              <div className='card-reveal'>
+                <span className='card-title grey-text text-darken-4'>
+                  Follow me
+                </span>
+                <p className='flex-container'>
+                  <i className='fab fa-facebook grey-text text-darken-4 social-style'></i>
+                  <i className='fab fa-linkedin grey-text text-darken-4 social-style'></i>
+                  <i className='fab fa-twitter grey-text text-darken-4 social-style'></i>
+                  <i className='fab fa-google-plus grey-text text-darken-4 social-style'></i>
+                </p>
+              </div>
+            </div>
+          </div>
         )
     }
 }
