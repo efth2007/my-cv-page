293d4d02e247852c4f487915a777c4a185e0f8d2
efth2007 <efth2007@hotmail.com>
2020-12-09 13:16:57 +0100

    Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,7 +3,69 @@ import React, { Component } from 'react'
 export default class Experience extends Component {
     render(){
         return(
-          <div></div>
+          <div>
+            <div className='card'>
+              <div className='card-content'>
+                <h6>
+                  WORK EXPERIENCE
+                </h6>
+                <div className='row'>
+                  <div className='col s12 m4 l4 xl4'>
+                    <p className='teal year_exp white-text'>
+                      Jan <strong>2011</strong> - May <strong>2015</strong>
+                    </p>
+                  </div>
+                  <div className='col s12 m8 l8 xl8'>
+                    <blockquote className='no-pad'>
+                      <h6 className='no-pad mt-bottom'>
+                        <strong>WEB DEVELOPMENT</strong>
+                      </h6>
+                      <p>Bla bla bla bla experience lorem ipsum bla bla bla lorem 
+                        ipsum text words. Experience words bla bla lorem ipsum 
+                        text words bla bla lorem ipsum words description of 
+                        experience test bla lorem ipsum experience words text.</p>
+                    </blockquote>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col s12 m4 l4 xl4'>
+                    <p className='teal year_exp white-text'>
+                      Jan <strong>2011</strong> - May <strong>2015</strong>
+                    </p>
+                  </div>
+                  <div className='col s12 m8 l8 xl8'>
+                    <blockquote className='no-pad'>
+                      <h6 className='no-pad mt-bottom'>
+                        <strong>WEB DEVELOPMENT</strong>
+                      </h6>
+                      <p>Bla bla bla bla experience lorem ipsum bla bla bla lorem 
+                        ipsum text words. Experience words bla bla lorem ipsum 
+                        text words bla bla lorem ipsum words description of 
+                        experience test bla lorem ipsum experience words text.</p>
+                    </blockquote>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col s12 m4 l4 xl4'>
+                    <p className='teal year_exp white-text'>
+                      Jan <strong>2011</strong> - May <strong>2015</strong>
+                    </p>
+                  </div>
+                  <div className='col s12 m8 l8 xl8'>
+                    <blockquote className='no-pad'>
+                      <h6 className='no-pad mt-bottom'>
+                        <strong>WEB DEVELOPMENT</strong>
+                      </h6>
+                      <p>Bla bla bla bla experience lorem ipsum bla bla bla lorem 
+                        ipsum text words. Experience words bla bla lorem ipsum 
+                        text words bla bla lorem ipsum words description of 
+                        experience test bla lorem ipsum experience words text.</p>
+                    </blockquote>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         )
     }
 }
