6cdae8590a7b89076c9767fe41a32df827ce4b17
efth2007 <efth2007@hotmail.com>
2020-12-09 12:58:19 +0100

    Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,65 @@ import React, { Component } from 'react'
 export default class Skills extends Component {
     render(){
         return(
-          <div></div>
+          <div>
+            <div className='card'>
+              <div className='card-content'>
+                <h6>
+                  <strong>PROFESSIONAL SKILLS</strong>
+                </h6>
+                <div className='row mt-top'>
+                  <div className='col s6'>
+                    <p>JAVASCRIPT</p>
+                  <div className='progress grey lighten-1'>
+                    <div className='determinate blue' style={ {width: '88%'}}>
+
+                    </div>
+                  </div>
+                </div>
+                  <div className='col s6'>
+                    <p>HTML</p>
+                  <div className='progress grey lighten-1'>
+                    <div className='determinate blue' style={ {width: '83%'}}>
+
+                    </div>
+                  </div>
+                </div>
+                  <div className='col s6'>
+                    <p>CSS</p>
+                  <div className='progress grey lighten-1'>
+                    <div className='determinate blue' style={ {width: '78%'}}>
+
+                    </div>
+                  </div>
+                </div>
+                  <div className='col s6'>
+                    <p>HTML</p>
+                  <div className='progress grey lighten-1'>
+                    <div className='determinate blue' style={ {width: '85%'}}>
+
+                    </div>
+                  </div>
+                </div>
+                  <div className='col s6'>
+                    <p>HTML</p>
+                  <div className='progress grey lighten-1'>
+                    <div className='determinate blue' style={ {width: '85%'}}>
+
+                    </div>
+                  </div>
+                </div>
+                  <div className='col s6'>
+                    <p>HTML</p>
+                  <div className='progress grey lighten-1'>
+                    <div className='determinate blue' style={ {width: '85%'}}>
+
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
         )
     }
 }
